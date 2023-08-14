6a0ae1993a6b83392b23ce620f3ffc792f8d8900
Akshay Bhasme <akshaybhasme30@gmail.com>
2023-08-14 23:23:37 +0530

    Update my_profile.py

--- my_profile.py
+++ my_profile.py
@@ -29,14 +29,23 @@ def pdf_github_to_images(pdf_github_url):
 # Function to display the resume
 def st_display_pdf(images,width=400, height=600):
     st.title("Career Snapshot")
-    st.write(f"Download My Resume here [PDF](https://github.com/Akshay-Bhasme/my_profile/blob/main/CV_Akshay_Bhasme.pdf)")
+    st.write(f"Download My Resume here [PDF](https://github.com/Akshay-Bhasme/my_profile/raw/main/CV_Akshay_Bhasme.pdf)")
     st.write(f"Email: akshaybhasme30@gmail.com          ",f"        Mobile No: +91 7972014093")
     st.write(f"GitHub: https://github.com/Akshay-Bhasme ")
     st.write(f"LinkedIn: www.linkedin.com/in/akshaybhasme30 ")
     for i, image in enumerate(images):
         st.image(image, caption=f"Page {i+1}", use_column_width=True)
-    st.write(f"Download My Resume here [PDF](https://github.com/Akshay-Bhasme/my_profile/blob/main/CV_Akshay_Bhasme.pdf)")
-   
+    st.write(f"Download My Resume here [PDF](https://github.com/Akshay-Bhasme/my_profile/raw/main/CV_Akshay_Bhasme.pdf)")
+    #response = requests.get(pdf_url)
+    #pdf_data = response.content
+    #pdf_b64 = base64.b64encode(pdf_data).decode("utf-8")
+    
+    # Use the iframe HTML tag to embed the PDF viewer
+    #st.markdown(f'<iframe src="data:application/pdf;base64,{pdf_b64}" width="800" height="600"></iframe>', unsafe_allow_html=True)
+    #with open(pdf_file,"rb") as f:
+    #    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    #pdf_display = F'<embed src=”data:application/pdf;base64,{base64_pdf}” width=”700″ height=”1000″ type=”application/pdf”>'
+    #st.markdown(pdf_display, unsafe_allow_html=True)
     
 # Function to display your work/projects
 #def display_work():
@@ -61,16 +70,9 @@ def main():
     choice = st.sidebar.radio("Go to", pages)
     
     if choice == "Resume":
-        pdf_github_url = "https://github.com/Akshay-Bhasme/my_profile/blob/main/CV_Akshay_Bhasme.pdf"  # Replace with your GitHub PDF URL
+        pdf_github_url = "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/CV_Akshay_Bhasme.pdf"  # Replace with your GitHub PDF URL
         images = pdf_github_to_images(pdf_github_url)
         st_display_pdf(images)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
