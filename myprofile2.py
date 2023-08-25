8360738f280283e9af66656c7bd806fdb434f600
Akshay Bhasme <akshaybhasme30@gmail.com>
2023-08-25 21:52:58 +0530

    Update myprofile2.py

--- myprofile2.py
+++ myprofile2.py
@@ -100,9 +100,75 @@ def main(page=None):
         images = pdf_github_to_images(pdf_github_url)
         st_display_pdf(images)
     elif page == "Courses and Certificates":
-        # ... (your existing code)
+        certificates = [
+            {
+                "course_name": "Applied Machine Learning Course",
+                "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/Applie%20AI.pdf",
+                "credentials": "https://www.appliedaicourse.com/certificate/902eadbdec"
+            },
+            {
+                "course_name": "Python (Basic) Certificate",
+                "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/python_basic%20certificate.pdf",
+                "credentials": "https://www.hackerrank.com/certificates/7ff0e9842ae8"
+            },
+            {
+                "course_name": "SQL (Basic) Certificate",
+                "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/sql_basic%20certificate.pdf",
+                "credentials": "https://www.hackerrank.com/certificates/1aaf4b4e8057"
+            },
+            {
+                "course_name": "Prompt Engineering for ChatGPT",
+                "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/PrompEngineeringForChatGPT.pdf",
+                "credentials": "https://www.coursera.org/account/accomplishments/certificate/3U8XGSFYA74H"
+            },
+            {
+                "course_name": "Hands-on Machine Learning with AWS and NVIDIA",
+                "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/HandsOnMachineLearning.pdf",
+                "credentials": "https://www.coursera.org/account/accomplishments/certificate/FYB8KV6CKWZH"
+            },
+            # Add more courses
+        ]
+        
+        for certificate in certificates:
+            certificate_pdf_url = certificate['certificate_pdf']
+            certificate_images = pdf_github_to_images(certificate_pdf_url)
+            if certificate_images:
+                certificate['certificate_image'] = certificate_images[0]  # Display only the first page as an image
+            else:
+                certificate['certificate_image'] = None
+            
+        st_display_certificates(certificates)
     elif page == "Blogs":
-        # ... (your existing code)
+        blogs = [
+            {
+                "title": "Exploring Emotions with BERT -Transfer Learning: Your Guide to Sentiment Analysis",
+                "read_more_link": "https://medium.com/@akshaybhasme30/exploring-emotions-with-bert-transfer-learning-your-guide-to-sentiment-analysis-6c260f9c1de5"
+            },
+            {
+                "title": "Unleashing the Power of Deep Learning: Exploring LSTM on the Donors Choose Dataset",
+                "read_more_link": "https://medium.com/@akshaybhasme30/unleashing-the-power-of-deep-learning-exploring-lstm-on-the-donors-choose-dataset-771951df6600"
+            },
+            {
+                "title": "Common Mistakes to Avoid in Model Building: Insights from a Data Scientist’s Journey",
+                "read_more_link": "https://medium.com/@akshaybhasme30/common-mistakes-to-avoid-in-model-building-insights-from-a-data-scientists-journey-dc3fbfb70925"
+            },
+            {
+                "title": "Principal component analysis (PCA)",
+                "read_more_link": "https://medium.com/@akshaybhasme30/principal-component-analysis-pca-d6de7a53efa7"
+            },
+            {
+                "title": "How to create Jupyter Notebook instance on Google Cloud Platform (GCP)",
+                "read_more_link": "https://medium.com/@akshaybhasme30/how-to-create-jupyter-notebook-instance-on-google-cloud-platform-gcp-3e74061dd869"
+            },
+            
+            # Add more blogs
+        ]
+        for blog in blogs:
+            title, image_url = fetch_medium_blog_info(blog['read_more_link'])
+            blog['title'] = title
+            blog['blog_image'] = Image.open(BytesIO(requests.get(image_url).content))
+            
+        st_display_blogs(blogs)
 
 if __name__ == "__main__":
     main()
