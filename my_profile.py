ef26398d265d6bf64d7ff140bc31136e1c74c344
Akshay Bhasme <akshaybhasme30@gmail.com>
2023-09-23 15:32:02 +0530

    Update my_profile.py

--- my_profile.py
+++ my_profile.py
@@ -136,22 +136,22 @@ def main():
             {
                 "title": "Exploring Emotions with BERT -Transfer Learning: Your Guide to Sentiment Analysis",
                 "read_more_link": "https://medium.com/@akshaybhasme30/exploring-emotions-with-bert-transfer-learning-your-guide-to-sentiment-analysis-6c260f9c1de5",
-                "blog_image": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/Exploring%20emotions.jpeg"
+                "blog_image": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/Exploringemotions.jpeg"
             },
             {
                 "title": "Unleashing the Power of Deep Learning: Exploring LSTM on the Donors Choose Dataset",
                 "read_more_link": "https://medium.com/@akshaybhasme30/unleashing-the-power-of-deep-learning-exploring-lstm-on-the-donors-choose-dataset-771951df6600",
-                "blog_image":"https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/unleashing%20the%20power.jpeg"
+                "blog_image":"https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/unleashingthepower.jpeg"
             },
             {
                 "title": "Exploring Linear Regression-OLS: Clearing Misconceptions Surrounding It",
                 "read_more_link": "https://medium.com/@akshaybhasme30/exploring-linear-regression-ols-clearing-misconceptions-surrounding-it-a5b21fe2c48a",
-                "blog_image": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/exploring%20linear%20reg.jpeg"
+                "blog_image": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/exploringlinearreg.jpeg"
             },
             {
                 "title": "Common Mistakes to Avoid in Model Building: Insights from a Data Scientist’s Journey",
                 "read_more_link": "https://medium.com/@akshaybhasme30/common-mistakes-to-avoid-in-model-building-insights-from-a-data-scientists-journey-dc3fbfb70925",
-                "blog_image":"https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/common%20mistakes.jpeg"
+                "blog_image":"https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/blog_images/commonmistakes.jpeg"
             },
             {
                 "title": "Principal component analysis (PCA)",
