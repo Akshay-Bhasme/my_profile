d57ab6af4a3bcb295a8efa52ce6d1b797295fb8d
Akshay Bhasme <akshaybhasme30@gmail.com>
2023-09-23 15:17:34 +0530

    Update my_profile.py

--- my_profile.py
+++ my_profile.py
@@ -99,39 +99,36 @@ def main():
             {
                 "course_name": "Applied Machine Learning Course",
                 "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/Applie%20AI.pdf",
-                "credentials": "https://www.appliedaicourse.com/certificate/902eadbdec"
+                "credentials": "https://www.appliedaicourse.com/certificate/902eadbdec",
+                'certificate_image':"https://github.com/Akshay-Bhasme/my_profile/blob/main/cert_images/appliedai.jpeg"
             },
             {
                 "course_name": "Python (Basic) Certificate",
                 "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/python_basic%20certificate.pdf",
-                "credentials": "https://www.hackerrank.com/certificates/7ff0e9842ae8"
+                "credentials": "https://www.hackerrank.com/certificates/7ff0e9842ae8",
+                'certificate_image':"https://github.com/Akshay-Bhasme/my_profile/blob/main/cert_images/python.jpeg"
             },
             {
                 "course_name": "SQL (Basic) Certificate",
                 "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/sql_basic%20certificate.pdf",
-                "credentials": "https://www.hackerrank.com/certificates/1aaf4b4e8057"
+                "credentials": "https://www.hackerrank.com/certificates/1aaf4b4e8057",
+                'certificate_image':"https://github.com/Akshay-Bhasme/my_profile/blob/main/cert_images/sql.jpeg"
             },
             {
                 "course_name": "Prompt Engineering for ChatGPT",
                 "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/PrompEngineeringForChatGPT.pdf",
-                "credentials": "https://www.coursera.org/account/accomplishments/certificate/3U8XGSFYA74H"
+                "credentials": "https://www.coursera.org/account/accomplishments/certificate/3U8XGSFYA74H",
+                'certificate_image':"https://github.com/Akshay-Bhasme/my_profile/blob/main/cert_images/gpt.jpeg"
             },
             {
                 "course_name": "Hands-on Machine Learning with AWS and NVIDIA",
                 "certificate_pdf": "https://raw.githubusercontent.com/Akshay-Bhasme/my_profile/main/HandsOnMachineLearning.pdf",
-                "credentials": "https://www.coursera.org/account/accomplishments/certificate/FYB8KV6CKWZH"
+                "credentials": "https://www.coursera.org/account/accomplishments/certificate/FYB8KV6CKWZH",
+                'certificate_image':"https://github.com/Akshay-Bhasme/my_profile/blob/main/cert_images/aws.jpeg"
             },
             # Add more courses
         ]
         
-        for certificate in certificates:
-            certificate_pdf_url = certificate['certificate_pdf']
-            certificate_images = pdf_github_to_images(certificate_pdf_url)
-            if certificate_images:
-                certificate['certificate_image'] = certificate_images[0]  # Display only the first page as an image
-            else:
-                certificate['certificate_image'] = None
-            
         st_display_certificates(certificates)
         
     elif choice == "Blogs":
